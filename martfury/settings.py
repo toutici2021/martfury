673f2b31e5076793b7d4ba2370f921ccfd25da59
franck-eric <61144472+franckeric96@users.noreply.github.com>
2021-03-23 16:18:00 +0100

    update

--- martfury/settings.py
+++ martfury/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%)_6$15$7bpf0fi+ma%#zv7nzweu=8@1^e9i)p4-*d8+2t%uk1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','furymart.herokuapp.com']
 
 
 # Application definition
